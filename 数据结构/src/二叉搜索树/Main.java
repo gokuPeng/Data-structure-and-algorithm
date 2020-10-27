dbf4e241775a73fe8eb3a9a66880a6be4cbd1251
pengyiming <1747670277@qq.com>
2020-10-27 22:47:18 +0800

    232

--- 数据结构/src/二叉搜索树/Main.java
+++ 数据结构/src/二叉搜索树/Main.java
@@ -4,43 +4,13 @@ import 二叉搜索树.printer.BinaryTrees;
 
 public class Main {
     public static void main(String[] args) {
-        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        BST binarySearchTree = new BST();
         Integer[] nums = {7, 4, 2, 1, 3, 5, 9, 8, 11, 10, 12};
         for (Integer num : nums) {
             binarySearchTree.add(num);
         }
         BinaryTrees.print(binarySearchTree);
         System.out.println();
-        binarySearchTree.preorder(new BinarySearchTree.Visitor<Integer>() {
-            @Override
-            boolean visit(Integer element) {
-                System.out.print(element + " ");
-                return false;
-            }
-        });
-        System.out.println();
-        binarySearchTree.inorder(new BinarySearchTree.Visitor<Integer>() {
-            @Override
-            boolean visit(Integer element) {
-                System.out.print(element + " ");
-                return false;
-            }
-        });
-        System.out.println();
-        binarySearchTree.postorder(new BinarySearchTree.Visitor<Integer>() {
-            @Override
-            boolean visit(Integer element) {
-                System.out.print(element + " ");
-                return false;
-            }
-        });
-        System.out.println();
-        binarySearchTree.levelOrder(new BinarySearchTree.Visitor<Integer>() {
-            @Override
-            boolean visit(Integer element) {
-                System.out.print(element + " ");
-                return false;
-            }
-        });
+        BinaryTrees.print(binarySearchTree);
     }
 }
