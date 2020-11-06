22769a2c07d89d60d3d4a9bd3a0ebde9d41fb9c4
pengyiming <1747670277@qq.com>
2020-11-06 13:58:02 +0800

    节点

--- 算法题/src/二叉树/TreeNode.java
+++ 算法题/src/二叉树/TreeNode.java
@@ -1,11 +1,14 @@
 package 二叉树;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
