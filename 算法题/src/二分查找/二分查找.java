8f2f799ae2f9fdfb5be66945fdb9a4ccdc1560d6
pengyiming <1747670277@qq.com>
2020-11-13 18:16:58 +0800

    704. 二分查找

--- 算法题/src/二分查找/二分查找.java
+++ 算法题/src/二分查找/二分查找.java
@@ -6,4 +6,21 @@ package 二分查找;
  */
 // https://leetcode-cn.com/problems/binary-search/
 public class 二分查找 {
+    public int search(int[] nums, int target) {
+        int begin = 0;
+        int end = nums.length;
+        while (begin < end) {
+            int cmp = (begin + end) >> 1;
+            if (target > nums[cmp]) {
+                begin = cmp + 1;
+            } else if (target < nums[cmp]){
+                end = cmp - 1;
+            } else {
+                return cmp;
+            }
+            System.out.println(begin);
+            System.out.println(end);
+        }
+        return -1;
+    }
 }
