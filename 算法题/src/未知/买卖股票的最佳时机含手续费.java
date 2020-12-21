b48ac96dae3da1c9a857722c94fa262aca9d6985
pengyiming <1747670277@qq.com>
2020-12-21 14:35:58 +0800

    714. 买卖股票的最佳时机含手续费

--- 算法题/src/未知/买卖股票的最佳时机含手续费.java
+++ 算法题/src/未知/买卖股票的最佳时机含手续费.java
@@ -5,64 +5,57 @@ package 未知;
  * @since 2020/12/17 18:58
  */
 public class 买卖股票的最佳时机含手续费 {
-    boolean flag = false;
-    int lastBuy = 0;
-
     public int maxProfit(int[] prices, int fee) {
         int ans = 0;
+        int buyPoint = -1;
+        int salePoint = -1;
+        // 是否进行过购买
+        int flag = 0;   // 0未购买1买了2卖了
         for (int i = 0; i < prices.length; i++) {
-            if (!flag) {
-                boolean buy = isBuy(i, prices, fee);
-                if (buy) {
-                    flag = !flag;
+            // 买点
+            // 如果是第一次直接买
+            if (flag == 0) {
+                buyPoint = i;
+                ans -= prices[i];
+                flag = 1;
+                continue;
+            } else if (flag == 1){
+                // 如果买过了
+                // 判断是不是价格更低
+                if (prices[i] < prices[buyPoint]) {
+                    // 如果价格更低则更换买点
+                    ans += (prices[buyPoint] - prices[i]);
+                    buyPoint = i;
+                    continue;
+                }
+            } else {
+                // 如果卖过了要比上次便宜fee块才能买
+                if (prices[i] + fee < prices[salePoint]) {
+                    flag = 1;
                     ans -= prices[i];
-                    lastBuy = prices[i];
-                    System.out.println(lastBuy);
-                }
-            } else {
-                boolean sale = isSale(i, prices, fee);
-                if (sale) {
-                    flag = !flag;
-                    ans += prices[i] - fee;
-                    System.out.println(prices[i]);
-                } else {
-                    if (prices[i] < lastBuy) {
-                        ans += lastBuy - prices[i];
-                        lastBuy = prices[i];
-                    }
+                    buyPoint = i;
+                    continue;
                 }
             }
-        }
-        return flag ? ans + lastBuy : ans;
-    }
 
-    private boolean isBuy(int index, int[] prices, int fee) {
-        for (int i = index + 1; i < prices.length; i++) {
-            if (prices[i] <= prices[i - 1]) {
-                return false;
-            } else {
-                return true;
+            // 卖点
+            if (flag == 1) {
+                // 如果比买价+fee高直接卖
+                if (prices[i] > prices[buyPoint] + fee) {
+                    ans += (prices[i] - fee);
+                    salePoint = i;
+                    flag = 2;
+                    System.out.println(salePoint);
+                }
+            } else if (flag == 2) {
+                // 卖过了，如果遇到比上次高的卖点，取消上次卖点，这次再卖
+                if (prices[i] > prices[salePoint]) {
+                    ans += (prices[i] - prices[salePoint]);
+                    salePoint = i;
+                    flag = 2;
+                }
             }
         }
-        return false;
-    }
-
-    private boolean isSale(int index, int[] prices, int fee) {
-        for (int i = index + 1; i < prices.length; i++) {
-            if (prices[i] + fee > prices[i - 1] && prices[i - 1] >= prices[index]) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-        if (flag) {
-            if (prices[prices.length - 1] >= lastBuy + fee) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
+        return flag == 1 ? ans + prices[buyPoint] : ans;
     }
 }
